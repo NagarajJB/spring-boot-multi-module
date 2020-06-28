04e81259db441a8d5445493835129acfa26fca8a
Nagaraja Balakrishna <nbalakrishna@manh.com>
2020-06-28 16:18:00 +0530

    Initial commit, Spring boot multi module project, configured build info and git info for actuator

---
+++ web-app-module/src/main/java/com/njb/SpringBootMultiModuleApplication.java
@@ -0,0 +1,13 @@
+package com.njb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMultiModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMultiModuleApplication.class, args);
+	}
+
+}
---
+++ web-app-module/src/test/java/com/njb/SpringBootMultiModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.njb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMultiModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
